244df1013ca87afa103a5e1d6459b1aeb026dae0
1azyCn <chester44311@hotmail.com>
2023-02-03 23:44:29 +0800

    Toolbar & Header Styling

--- src/app/page/dashboard/dashboard.component.ts
+++ src/app/page/dashboard/dashboard.component.ts
@@ -1,21 +1,40 @@
-import { Component } from '@angular/core';
-import { Column } from 'src/app/components/table/Column';
+import { Component, OnInit } from '@angular/core';
+import { Column } from 'src/app/model/Column';
 
-import { Employee } from './Employee';
+import { Employee } from '../../model/Employee';
+
+import { GetAllEmployeeRecordsService } from 'src/app/service/getAllEmployeeService/get-all-employee-records.service';
 
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
   styleUrls: ['./dashboard.component.css']
 })
-export class DashboardComponent {
+export class DashboardComponent implements OnInit{
 
-    employeeRecords: Array<Employee> = []
+    employeeRecords!: Employee[]
 
     employeeRecordsColumn: Array<Column> = [
-        {columnDef: 'id', header: 'Id', cell: (element: Record<string, any>) => `${element['id']}`},
-        {columnDef: 'login', header: 'Login', cell: (element: Record<string, any>) => `${element['login']}`},
-        {columnDef: 'name', header: 'Name', cell: (element: Record<string, any>) => `${element['name']}`},
-        {columnDef: 'salary', header: 'Salary', cell: (element: Record<string, any>) => `${element['salary']}`},
+        {columnDef: 'id', header: 'Id', cell: (element: Record<string, any>) => this.employeeRecords[0].id},
+        {columnDef: 'login', header: 'Login', cell: (element: Record<string, any>) => element['login']},
+        {columnDef: 'name', header: 'Name', cell: (element: Record<string, any>) =>element['name']},
+        {columnDef: 'salary', header: 'Salary', cell: (element: Record<string, any>) => element['salary']},
     ]
+
+    constructor (private service:GetAllEmployeeRecordsService) {}
+
+    ngOnInit(): void {
+        this.service.getAllEmployeeRecordsBeanService().subscribe(
+          response => this.handleResponse(response)
+        )
+    }
+
+    // handle response from get
+    handleResponse(res : any) {
+      console.log('this is from get all employee records ', res);
+      this.employeeRecords = res.data.list;
+
+      console.log('employeerec', this.employeeRecords);
+      console.log('employeecol ', this.employeeRecordsColumn);
+    }
 }
